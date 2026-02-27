9baae755f1975fb83900fc4263544d763dda3c72
Roberto H luna <roberto@lunivate.com>
2026-02-27 22:09:57 +0700

    Update osagent to v0.2.4

--- Formula/osagent.rb
+++ Formula/osagent.rb
@@ -1,28 +1,28 @@
 class Osagent < Formula
   desc "Signal Theory-optimized AI agent - your OS, supercharged"
   homepage "https://github.com/Miosa-osa/OSA"
-  version "0.2.3"
+  version "0.2.4"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
       url "https://github.com/Miosa-osa/OSA/releases/download/v#{version}/osagent-#{version}-darwin-arm64.tar.gz"
-      sha256 "2e86575e31e8c0fa4bd57f7778e30ad9eb7235b2e6745f45aaee23f4a35fca72"
+      sha256 "de14cb63e6026b4425ea50f3aeef662ee49e9f1f0bed9e4a7aa6a41abf5a557d"
     end
     on_intel do
       url "https://github.com/Miosa-osa/OSA/releases/download/v#{version}/osagent-#{version}-darwin-amd64.tar.gz"
-      sha256 "fe0df7c5780f5fdbc0f6450b603a84140ae63d8879c458c1640658f0d6833df0"
+      sha256 "c3de8a0f19ade4beb3138076f287f194f8f4e348b1cb92ac1fcaee768f0f51eb"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/Miosa-osa/OSA/releases/download/v#{version}/osagent-#{version}-linux-arm64.tar.gz"
-      sha256 "2e4c303e5d5bc96ca8835520d5bf82570136b93ea82e78486a9f1ba9cdb659bf"
+      sha256 "e17e845fdfe506407c89640689872dd13c3523febbf96eac81151554567c778b"
     end
     on_intel do
       url "https://github.com/Miosa-osa/OSA/releases/download/v#{version}/osagent-#{version}-linux-amd64.tar.gz"
-      sha256 "85aa06f98a01a91081eec10e9a538e0aae5faac0f5081a02407e28e8a77e9dd8"
+      sha256 "bda79bb3633910902c5b76687168f33ec5ce601e3f2718327cde345a90c06aa6"
     end
   end
 
