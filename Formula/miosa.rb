0afbb804f70fefb191e23abf70982e8c1082122e
Roberto H luna <roberto@lunivate.com>
2026-05-01 23:05:38 -0500

    fix: host miosa artifact on public tap release

--- Formula/miosa.rb
+++ Formula/miosa.rb
@@ -1,8 +1,8 @@
 class Miosa < Formula
   desc "MIOSA CLI for OpenComputers, deployments, machines, and tunnels"
   homepage "https://github.com/Miosa-osa/miosa"
-  url "https://github.com/Miosa-osa/miosa/releases/download/miosa-cli-v0.1.0/miosa_0.1.0_node.tar.gz"
-  sha256 "9540718620674848a0cab74e44c041d6ae5a07974d9ba52cd44173d62ed54462"
+  url "https://github.com/Miosa-osa/homebrew-tap/releases/download/miosa-cli-v0.1.0/miosa_0.1.0_node.tar.gz"
+  sha256 "5d2cadee34a737c0c2b5639757bc8467687fdc7c10edf48b23cf896bec9b1d10"
   version "0.1.0"
   license "Apache-2.0"
 
