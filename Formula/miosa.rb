f4ccd48a47c1314e95bc0633906d9050c1630a92
Roberto H luna <roberto@lunivate.com>
2026-07-20 23:18:40 -0500

    miosa 1.2.2

--- Formula/miosa.rb
+++ Formula/miosa.rb
@@ -1,26 +1,26 @@
 class Miosa < Formula
   desc "Official CLI for MIOSA sandboxes"
   homepage "https://miosa.ai"
-  version "1.2.0"
+  version "1.2.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Miosa-osa/miosa-cli-go/releases/download/v1.2.0/miosa_1.2.0_darwin_arm64.tar.gz"
-      sha256 "f3cffa77e4b8519bad694d895ac75e79c2e2b70e7d9cd0c7bd79b9cb110aa10b"
+      url "https://github.com/Miosa-osa/miosa-cli-go/releases/download/v1.2.2/miosa_1.2.2_darwin_arm64.tar.gz"
+      sha256 "64e91a2ba161d9d41f98257b35de724d90b81d03f649a603de59699d203574b7"
     else
-      url "https://github.com/Miosa-osa/miosa-cli-go/releases/download/v1.2.0/miosa_1.2.0_darwin_amd64.tar.gz"
-      sha256 "6d45db29411c9a9ebc3a26c26cce7ab8b42a9dd9a799fd29526e5e9436009fb7"
+      url "https://github.com/Miosa-osa/miosa-cli-go/releases/download/v1.2.2/miosa_1.2.2_darwin_amd64.tar.gz"
+      sha256 "2306b0f58573c1f9bfa7960a40bdc5cef21debd6203db52cbcd01afcb45dc7e6"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/Miosa-osa/miosa-cli-go/releases/download/v1.2.0/miosa_1.2.0_linux_arm64.tar.gz"
-      sha256 "ef01c02f5d5416b6ea63bde01f7ddf7af3d0d7b8798c695b2e0603066e78de0c"
+      url "https://github.com/Miosa-osa/miosa-cli-go/releases/download/v1.2.2/miosa_1.2.2_linux_arm64.tar.gz"
+      sha256 "744e15056b76863e8932789ac5bc63f8609eb8e53c64685c6a0840a0504eab3b"
     else
-      url "https://github.com/Miosa-osa/miosa-cli-go/releases/download/v1.2.0/miosa_1.2.0_linux_amd64.tar.gz"
-      sha256 "da40b4e215bfbee3d008dece10cf8f9c85da08d7bada83077f97b91ba6fecaad"
+      url "https://github.com/Miosa-osa/miosa-cli-go/releases/download/v1.2.2/miosa_1.2.2_linux_amd64.tar.gz"
+      sha256 "18a390c8e7d1b9e108f76d3ba8a9758ea0a93d7d3e11956c5a8470c0912d57b9"
     end
   end
 
