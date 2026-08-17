9cef50f1a527f9347a0fd39b057e3b6f2172ed8a
Roberto H luna <roberto@lunivate.com>
2026-08-17 07:26:11 -0500

    release: update MIOSA formula to 1.2.4

--- Formula/miosa.rb
+++ Formula/miosa.rb
@@ -1,26 +1,26 @@
 class Miosa < Formula
-  desc "Official CLI for MIOSA sandboxes"
+  desc "Official CLI for MIOSA cloud and Forge"
   homepage "https://miosa.ai"
-  version "1.2.2"
+  version "1.2.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Miosa-osa/miosa-cli-go/releases/download/v1.2.2/miosa_1.2.2_darwin_arm64.tar.gz"
-      sha256 "64e91a2ba161d9d41f98257b35de724d90b81d03f649a603de59699d203574b7"
+      url "https://github.com/Miosa-osa/miosa-cli-go/releases/download/v1.2.4/miosa_1.2.4_darwin_arm64.tar.gz"
+      sha256 "5611a799e3bd519709951bb87a56c2b659831670b007184ffe315a4d9b260f9f"
     else
-      url "https://github.com/Miosa-osa/miosa-cli-go/releases/download/v1.2.2/miosa_1.2.2_darwin_amd64.tar.gz"
-      sha256 "2306b0f58573c1f9bfa7960a40bdc5cef21debd6203db52cbcd01afcb45dc7e6"
+      url "https://github.com/Miosa-osa/miosa-cli-go/releases/download/v1.2.4/miosa_1.2.4_darwin_amd64.tar.gz"
+      sha256 "c0fb7b7dd1fc24c059bc5d4d54edbe2e1b6339c475efa2582ad12bb3b7020c41"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/Miosa-osa/miosa-cli-go/releases/download/v1.2.2/miosa_1.2.2_linux_arm64.tar.gz"
-      sha256 "744e15056b76863e8932789ac5bc63f8609eb8e53c64685c6a0840a0504eab3b"
+      url "https://github.com/Miosa-osa/miosa-cli-go/releases/download/v1.2.4/miosa_1.2.4_linux_arm64.tar.gz"
+      sha256 "a3aca95ee05c4ab05e9f4fcb86eeb8632542c59d2a277b610e9489e27c35d243"
     else
-      url "https://github.com/Miosa-osa/miosa-cli-go/releases/download/v1.2.2/miosa_1.2.2_linux_amd64.tar.gz"
-      sha256 "18a390c8e7d1b9e108f76d3ba8a9758ea0a93d7d3e11956c5a8470c0912d57b9"
+      url "https://github.com/Miosa-osa/miosa-cli-go/releases/download/v1.2.4/miosa_1.2.4_linux_amd64.tar.gz"
+      sha256 "20003034031dae944283616bcf970bd4d78b662563dfebe7b9f6f2f63e2534a9"
     end
   end
 
